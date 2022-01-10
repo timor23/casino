194b141e2973394f9b444fd0ee41cecf33f21a85
timor23 <timors23@protonmail.com>
2022-01-10 10:04:50 +0200

    front

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,18 +1,18 @@
-import { Redirect, Route } from "react-router-dom"
+import {Redirect, Route} from "react-router-dom"
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        localStorage.getItem("authToken") ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/login" />
-        )
-      }
-    />
-  );
-};
+const PrivateRoute = ({component: Component, ...rest}) => {
+    return (
+        <Route
+            {...rest}
+            render={(props) =>
+                localStorage.getItem("authToken") ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to="/login"/>
+                )
+            }
+        />
+    )
+}
 
 export default PrivateRoute;
