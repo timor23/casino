2e3b582200baa7f482207a3892b4a1e86821e15d
timor23 <timors23@protonmail.com>
2022-01-10 09:57:07 +0200

    front

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require('bcryptjs')
+const ErrorResponse = require('../utils/errorResponse')
+const sendEmail = require("../utils/sendEmail")
 
 exports.register = async (req, res, next) => {
     const {userName, email, password} = req.body
@@ -11,15 +13,9 @@ exports.register = async (req, res, next) => {
             password
         })
 
-        res.status(201).json({
-            success: true,
-            user
-        })
+        sendToken(user, 201, res)
     } catch (e) {
-        res.status(500).json({
-            success: false,
-            error: e.message
-        })
+        next(e)
     }
 
 }
@@ -28,50 +24,84 @@ exports.login = async (req, res, next) => {
     const {email, password} = req.body
 
     if (!email || !password) {
-        res.status(400).json({
-            success: false,
-            error: 'please provide amil and password'
-        })
+        return next(new ErrorResponse('please provide amil and password', 400))
     }
 
     try {
         const user = await User.findOne({email}).select('+password')
 
         if (!user) {
-            res.status(400).json({
-                success: false,
-                error: 'Invalid credentials'
-            })
+            return next(new ErrorResponse('Invalid credentials', 401))
         }
 
         const isMatch = await user.matchPasswords(password)
 
         if (!isMatch) {
-            res.status(400).json({
-                success: false,
-                error: 'Invalid credentials'
-            })
+            return next(new ErrorResponse('Invalid credentials', 401))
         }
 
-        res.status(200).json({
-            success: true,
-            token: "fdhfedy6tgj",
-            user
-        })
+        sendToken(user, 200, res)
 
-    } catch (e){
-        res.status(500).json({
-            success: false,
-            error: e.message
-        })
+    } catch (e) {
+        next(e)
     }
 }
 
-exports.forgotPassword = (req, res, next) => {
-    res.send('Forgot Password Route')
+exports.forgotPassword = async (req, res, next) => {
+    const {email} = req.body
+
+    try {
+        const user = await User.findOne({email})
+
+        // check if user exists
+        if (!user) {
+            return next(new ErrorResponse("Email could not be sent", 404))
+        }
+
+        // Reset Token Gen and add to database hashed (private) version of token
+        const resetToken = user.getResetPasswordToken()
+
+        await user.save()
+
+        // Create reset url to email to provided email
+        const resetUrl = `http://localhost:3000/passwordreset/${resetToken}`
+
+        // HTML Message
+        const message = `
+      <h1>You have requested a password reset</h1>
+      <p>Please make a put request to the following link:</p>
+      <a href=${resetUrl} clicktracking=off>${resetUrl}</a>`
+
+        try {
+            await sendEmail({
+                to: user.email,
+                subject: "Password Reset Request",
+                text: message,
+            });
+
+            res.status(200).json({success: true, data: "Email Sent"});
+        } catch (err) {
+            console.log(err);
+
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpire = undefined;
+
+            await user.save();
+
+            return next(new ErrorResponse("Email could not be sent", 500));
+        }
+    } catch (err) {
+        next(err);
+    }
+
 }
 
 exports.resetPassword = (req, res, next) => {
     res.send('Reset Password Route')
+}
+
+const sendToken = (user, statusCode, res) => {
+    const token = user.getSignedToken()
+    res.status(statusCode).json({success: true, token})
 }
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config({path: './config.env'})
 const express = require('express')
 const connectDB = require("./config/db")
+const errorHandler = require('./middleware/error')
 const app = express()
 // const users = require("./routes/users.route");
 
@@ -11,7 +12,9 @@ const PORT = process.env.PORT || 5001
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
+app.use('/api/private', require('./routes/private'))
 
+app.use(errorHandler)
 // app.use(users)
 
 app.listen(PORT, () => {
